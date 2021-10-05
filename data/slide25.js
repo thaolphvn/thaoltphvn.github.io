d47f135a85d3eac0736093bdc6bc5ad9813e1f00
dungtahvn <91324502+dungtahvn@users.noreply.github.com>
2021-10-05 13:42:25 +0700

    upload

---
+++ data/slide25.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{A8B63CBC-8B5D-4939-BF76-890DD67A6FE2}'];loadHandler&&loadHandler(24, '<div id="spr0_4f4b9d2"><div id="spr1_4f4b9d2" class="kern slide"><img id="img0_4f4b9d2" src="data/img97.jpg" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_4f4b9d2" class="kern slide"></div></div>', '{"s":[]}');})();
